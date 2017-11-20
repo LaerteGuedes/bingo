0e68d457275a6d09e3867a7662dbfdedefdc6d50
LaerteGM <laerteguedes8@gmail.com>
2017-11-19 23:27:19 -0300

    Starting layout templating

--- web/src/main/java/br/com/bingo/web/config/BingoApplication.java
+++ web/src/main/java/br/com/bingo/web/config/BingoApplication.java
@@ -4,16 +4,24 @@ import br.com.bingo.core.config.JpaConfig;
 import br.com.bingo.core.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
 @Import({JpaConfig.class, SecurityConfig.class})
 @ComponentScan(basePackages = {"br.com.bingo.web.controllers", "br.com.bingo.core.services"})
-public class BingoApplication {
+@EnableWebMvc
+public class BingoApplication extends WebMvcAutoConfiguration{
 
     public static void main(String[] args) {
         SpringApplication.run(BingoApplication.class, args);
     }
+
 }
